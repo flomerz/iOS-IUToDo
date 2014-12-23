ce8bc19c9a3c4fbf8d1631002d2d65e78545416a
Flo <flo@Flos-Mac.local>
2014-12-23 17:49:58 +0100

    initial version

---
+++ IUToDo/ToDo.swift
@@ -0,0 +1,24 @@
+//
+//  ToDo.swift
+//  IUToDo
+//
+//  Created by Flo on 22/12/14.
+//  Copyright (c) 2014 Flo. All rights reserved.
+//
+
+import Foundation
+
+class ToDo {
+    var title: String
+    var subject: String
+    var doneDate: NSDate?
+    
+    init (title: String, subject: String) {
+        self.title = title
+        self.subject = subject
+    }
+    
+    func done() {
+        doneDate = NSDate()
+    }
+}
